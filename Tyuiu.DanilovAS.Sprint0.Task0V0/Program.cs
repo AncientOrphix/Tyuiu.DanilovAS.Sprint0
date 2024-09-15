718a8d2d49b3260e7d502936cf0868fce7376872
Tyuiu.DanilovAlexander <Tyuiu.DanilovAlexander@outlook.com>
2024-09-15 13:16:37 +0500

    Add project files.

---
+++ Tyuiu.DanilovAS.Sprint0.Task0V0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.DanilovAS.Sprint0.Task0V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
