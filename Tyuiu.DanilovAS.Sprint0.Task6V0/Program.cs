a6bc84a995f8ba9253d6599d52b0eb0636762208
Tyuiu.DanilovAlexander <Tyuiu.DanilovAlexander@outlook.com>
2024-09-15 14:48:02 +0500

    Task6

---
+++ Tyuiu.DanilovAS.Sprint0.Task6V0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using Tyuiu.DanilovAS.Sprint0.Task6V0.Lib;
+
+namespace Tyuiu.DanilovAS.Sprint0.Task6V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraynums = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(arraynums));
+            Console.WriteLine("Разность элементов массива = " + DataService.SubtrationArray(arraynums));
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(arraynums));
+            Console.ReadKey();
+        }
+    }
+}
